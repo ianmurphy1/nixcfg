4e4919117bd73492dca9bceb155bfdebaf643f9c
ianmurphy1 <iano200@gmail.com>
2024-11-06 10:01:19 +0000

    update with template

--- hosts/nixdarwin/packages/zsh/default.nix
+++ hosts/nixdarwin/packages/zsh/default.nix
@@ -1,24 +1,24 @@
 { pkgs, config, ... }:
 {
-sops.secrets = {
-  test_secret = {
-    owner = "ian";
+  sops.secrets = {
+    test_secret = {
+      owner = "ian";
+    };
   };
-};
   environment.systemPackages = with pkgs; [
     zoxide
     zsh-powerlevel10k
     zsh-fast-syntax-highlighting
   ];
 
-  #sops.templates.test_template = {
-  #  content = ''
-  #    TESTING OUT SOPS TEMPLATES
-  #    THE SECRET VALUE IS: ${config.sops.placeholder.test_template_secret}
-  #    TESTING IF THIS GETS ADDED
-  #  '';
-  #  owner = "ian";
-  #};
+  sops.templates.test_template = {
+    content = ''
+      TESTING OUT SOPS TEMPLATES
+      THE SECRET VALUE IS: ${config.sops.placeholder.test_secret}
+      TESTING IF THIS GETS ADDED
+    '';
+    owner = "ian";
+  };
 
   environment.variables = {
     VAULT_ADDR = "https://vault.home";
