9e156dd4ae313b215ace2f5c32623c81f7cc1160
ianmurphy1 <iano200@gmail.com>
2024-11-06 17:52:18 +0000

    switch vault to use prebuilt bin instead of building locally

--- hosts/common/cli/default.nix
+++ hosts/common/cli/default.nix
@@ -3,7 +3,7 @@
   # Collection of cli tools that didn't really fit
   # into a specific place
   environment.systemPackages = with pkgs; [
-    vault jq yq-go fastfetch 
+    vault-bin jq yq-go fastfetch 
     bat step-cli zip 
     unrar unzip sops age
     tree ripgrep fd
