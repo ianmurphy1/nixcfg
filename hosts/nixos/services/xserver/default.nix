ffc869e36e38276338eb5e2e128fb26a22d25440
ianmurphy1 <iano200@gmail.com>
2025-05-19 21:01:51 +0100

    Disable xserver on nixos

--- hosts/nixos/services/xserver/default.nix
+++ hosts/nixos/services/xserver/default.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 {
   services.xserver = {
-    enable = true;
+    enable = false;
     exportConfiguration = true;
     xkb = {
       layout = "gb";
     };
   };
+  services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 }
