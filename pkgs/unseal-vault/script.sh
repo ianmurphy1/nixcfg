b3d6f83ef9dda37d6ac53e25b82dfe7240eca5a5
ianmurphy1 <iano200@gmail.com>
2025-05-25 09:13:10 +0100

    Fix formatting in script

--- pkgs/unseal-vault/script.sh
+++ pkgs/unseal-vault/script.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Script to unseal vault when it restarts
 # Take 3 random keys from a file and curl the server
 # to unseal
@@ -10,12 +11,12 @@ TOKEN_FILE=${TOKEN_FILE:?"needs to be set"}
 TOKENS=($(shuf -n 3 "${TOKEN_FILE}"))
 
 for t in ${TOKENS[@]}; do
-	_body=$(jq -c -n --arg token $t '{"key": $token}')
-	curl -s \
-		-f \
+  _body=$(jq -c -n --arg token $t '{"key": $token}')
+  curl -s \
+    -f \
     -w "" \
-		-o /dev/null \
-		-X POST \
-		-d "${_body}" \
-		"${VAULT_ADDR}/v1/sys/unseal"
+    -o /dev/null \
+    -X POST \
+    -d "${_body}" \
+    "${VAULT_ADDR}/v1/sys/unseal"
 done
