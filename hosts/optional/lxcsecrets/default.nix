b1f4539ef42f028c8e4bb92a7c2bf41c35fd8296
ianmurphy1 <iano200@gmail.com>
2025-12-18 08:34:59 +0000

    format

--- hosts/optional/lxcsecrets/default.nix
+++ hosts/optional/lxcsecrets/default.nix
@@ -3,6 +3,6 @@
 let
   lxcsecrets = pkgs.writeShellScriptBin "lxcsecrets" (builtins.readFile ./scripts/lxc_secrets.sh);
 in 
-  {
-    environment.systemPackages = [ lxcsecrets ];
-  }
+{
+  environment.systemPackages = [ lxcsecrets ];
+}
