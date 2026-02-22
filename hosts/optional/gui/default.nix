6f849698b68cfa0780941a00cae3d9ce3ecdd84a
ianmurphy1 <iano200@gmail.com>
2026-02-22 08:07:39 +0000

    Add in beekeeper settings

--- hosts/optional/gui/default.nix
+++ hosts/optional/gui/default.nix
@@ -4,4 +4,16 @@
     tutanota-desktop
     beekeeper-studio
   ];
+
+  environment.etc = {
+    "beekeeper-studio/system.config.ini" = {
+      text = ''
+        ; My Beekeeper Studio Configuration
+        ; Lines starting with semicolons are comments
+
+        [ui.tableTable]
+        pageSize = 1000
+      '';
+    };
+  };
 }
