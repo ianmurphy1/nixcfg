c52bf4f32c1b0c428a956183c79d5c0e8efaceaa
ianmurphy1 <iano200@gmail.com>
2026-05-07 06:47:18 +0100

    Update format

--- pkgs/randpass/default.nix
+++ pkgs/randpass/default.nix
@@ -1,23 +1,24 @@
 {
   rustPlatform,
 }:
-  rustPlatform.buildRustPackage (finalAttrs: {
-    pname = "randpass";
-    version = "0.0.1";
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "randpass";
+  version = "0.0.1";
 
-    src = fetchGit {
-      url = "git@github.com:ianmurphy1/randpass.git";
-      ref = "main";
-      rev = "491c1e0d7bc0402e856c4a0d0f073a0f0659e94c";
-    };
+  src = fetchGit {
+    url = "git@github.com:ianmurphy1/randpass.git";
+    ref = "main";
+    rev = "491c1e0d7bc0402e856c4a0d0f073a0f0659e94c";
+  };
 
-    cargoHash = "sha256-aL6PO5hXCRWNvl3c8/oOs8BqbQAb82Yf4ziQJcT42Yk=";
+  cargoHash = "sha256-aL6PO5hXCRWNvl3c8/oOs8BqbQAb82Yf4ziQJcT42Yk=";
 
   meta = {
-    description = "Utility that combines the usability of The Silver Searcher with the raw speed of grep";
+    description = "Small cli to generate random passwords";
     homepage = "https://github.com/ianmurphy1/randpass";
     maintainers = [
       "ianm"
     ];
   };
+
 })
