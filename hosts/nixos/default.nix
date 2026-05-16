f9654081c317aa24b2c6d3bf98b81f1946f72e03
ianmurphy1 <iano200@gmail.com>
2026-05-16 12:45:27 +0100

    Update flake

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -120,7 +120,7 @@ in
         "https://cache.nixos.org"
       ];
       trusted-public-keys = [
-        "ncps.home:zlSfRlVSyH3gjGcHZXSyUqT8l2ffqzncwh/GSEpf5AY="
+        "ncps.home:fQGhQtLXsh03vvfeXpu/My29QcpjxC+de46oYqNwzKQ="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
     };
