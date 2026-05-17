f1854208cb1b621068f032a81a5d9b332419a06e
ianmurphy1 <iano200@gmail.com>
2026-05-17 13:31:16 +0100

    Update flakes

--- hosts/optional/nh/default.nix
+++ hosts/optional/nh/default.nix
@@ -1,10 +1,22 @@
-{ config, myvars, ... }:
+{ config, myvars, pkgs, ... }:
 let
   homedir = config.users.users."${myvars.username}".home;
+  nh-latest = pkgs.nh.overrideAttrs(
+    finalAttrs: previousAttrs: {
+      version = "master";
+      src = pkgs.fetchFromGitHub {
+        owner = "nix-community";
+        repo = "nh";
+        rev = "247d26d2058eda0ca26315cac8cb415ea599927f";
+        hash = "sha256-B14CmTmyokfvM/ADMTyMpaH2Ip8Gtyc35pLpocVf6X4=";
+      };
+    }
+  );
 in
 {
   programs.nh = {
     enable = true;
+    package = nh-latest;
     flake = "${homedir}/nixcfg";
     clean = {
       enable = true;
