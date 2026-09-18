a964cbce8887c08c7f8609ccf1076acb17c0386a
ianmurphy1 <iano200@gmail.com>
2026-09-18 07:54:25 +0100

    update flakes

--- hosts/legion/default.nix
+++ hosts/legion/default.nix
@@ -135,9 +135,6 @@ in
 
   nixpkgs = {
     overlays = [
-      # (final: prev: {
-      #   buildGo125Module = prev.buildGoModule;
-      # })
       (final: prev: {
         unstable = import inputs.nixpkgs-unstable {
           system = system;
